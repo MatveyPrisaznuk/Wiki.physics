939478238a24f60bcc63f8b496444774ae5b43f7
MatveyPrisaznuk <matveyprys@gmail.com>
2023-12-03 18:10:43 +0200

    3/12/23

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for, redirect
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def page():
+    return render_template('page.html')
+
+@app.route('/author')
+def author():
+    return render_template('author.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
